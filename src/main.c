87c9b25cdd167943f6b20682483c914024fec7fb
Sleepy105 <lm.sousa@fe.up.pt>
2018-11-15 20:00:41 +0000

    Bug Fixes and minor cosmetic changes

--- src/main.c
+++ src/main.c
@@ -33,7 +33,7 @@ volatile uint8_t stateBeforeEm = 0;
 uint8_t Emergency = FALSE;
 
 volatile uint8_t stateRegular = 0;
-volatile int remainingMillis = 0;         // Count elapsed milliseconds from last state change
+volatile int remainingMillis = 0;       // Count elapsed milliseconds from last state change
 
 ISR(INT0_vect) {                        // Enter 'Emergency' mode on button press
     if (!Emergency) {
@@ -43,21 +43,23 @@ ISR(INT0_vect) {                        // Enter 'Emergency' mode on button pres
 }
 
 ISR(TIMER1_COMPA_vect) {
-    remainingMillis -= 10;
+    if (remainingMillis) {
+        remainingMillis -= 10;
+    }
 }
 
-void tc1_init() {
+void tc1_init() {                       // Initialize Timer1 to trigger a interrupt every 10ms
     TCCR1B = 0;                         // STOP the timer
     TIFR1 = (7<<TOV1) | (1<<ICF1);      // Clear flag register
     TCCR1A = 0;                         // Set mode...
     TCCR1B = (1<<WGM12);                // ...to CTC
     OCR1A = 625;                        // OCR1A
     TIMSK1 = (1<<OCIE1A);               // Enable interrupt on compare with OCR1A
-    TCCR1B |= 4;                        // Set the TP to 256
+    TCCR1B |= 4;                        // Set the TP to 256, therefore also starting the timer
 }
 
 void hardware_Init() {
-    /* Stoplights*/
+    /* Stoplights */
     DDRB |= (1 << RNS);                 // NS red
     DDRB |= (1 << YNS);                 // NS yellow
     DDRB |= (1 << GNS);                 // NS green
@@ -65,6 +67,7 @@ void hardware_Init() {
     DDRB |= (1 << YEW);                 // EW yellow
     DDRB |= (1 << GEW);                 // EW green
 
+    /* EMG_BUTTON as input */
     DDRD &= ~(1 << EMG_BUTTON);         // Set as Input
     PORTD |= (1 << EMG_BUTTON);         // Activate the internal Pull-up
 
@@ -72,7 +75,8 @@ void hardware_Init() {
     EICRA |= (0x02);                    // INT0 Interrupt at FE
     EIMSK |= (1 << INT0);
 
-      sei();
+    /* Activate Interrupts */
+    sei();
 }
 
 void disableExternalInputs() {
@@ -85,7 +89,7 @@ int main() {
     /* INIT Timers */
     tc1_init();
 
-    remainingMillis=RED_DELAY;
+    remainingMillis = RED_DELAY;
   
     
     while (1) {
@@ -152,9 +156,9 @@ int main() {
                 PORTB = (1 << YNS);
                 break;
             case 7:
-                if (BREAKDOWN_DELAY <= remainingMillis) {
+                if (0 == remainingMillis) {
                     stateRegular = 6;
-                    remainingMillis =  BREAKDOWN_DELAY;  // Reset counter
+                    remainingMillis = BREAKDOWN_DELAY;  // Reset counter
                 }
 
                 PORTB = (1 << YEW);
@@ -164,33 +168,43 @@ int main() {
 
             /* EMERGENCY STATES */
             case 8:
-                if ((1 == stateBeforeEm || 2 == stateBeforeEm) && !Emergency) {
-                    stateRegular = 9;
-                    remainingMillis=YELLOW_DELAY;
-                }
-                else if ((4 == stateBeforeEm || 5 == stateBeforeEm) && !Emergency) {
-                    stateRegular = 10;
-                    remainingMillis=YELLOW_DELAY;
+                if (!Emergency) {
+                    switch(stateBeforeEm) {
+                        case 1:
+                            remainingMillis = YELLOW_DELAY;
+                        case 2:
+                            stateRegular = 9;
+                            break;
+                        case 4:
+                            remainingMillis = YELLOW_DELAY;
+                        case 5:
+                            stateRegular = 10;
+                            break;
+                        default:
+                            stateRegular = 11;
+                            remainingMillis = STOPPED_DELAY;
+                            break;
+                    }
                 }
                 else {
                     stateRegular = 11;
-                    remainingMillis=RED_DELAY;
+                    remainingMillis = STOPPED_DELAY;
                 }
 
                 Emergency = TRUE;
                 break;
             case 9:
-                if (0== remainingMillis) {
+                if (0 == remainingMillis) {
                     stateRegular = 11;
-                    remainingMillis = RED_DELAY;
+                    remainingMillis = STOPPED_DELAY;
                 }
 
                 PORTB = (1 << YNS) | (1 << REW);
                 break;
             case 10:
-                if (0== remainingMillis) {
+                if (0 == remainingMillis) {
                     stateRegular = 11;
-                    remainingMillis = RED_DELAY;
+                    remainingMillis = STOPPED_DELAY;
                 }
 
                 PORTB = (1 << RNS) | (1 << YEW);
@@ -199,15 +213,15 @@ int main() {
                 if (0 == remainingMillis) {
                     if (1 == stateBeforeEm || 2 == stateBeforeEm) {
                         stateRegular = 4;
-                        remainingMillis=GREEN_DELAY;
+                        remainingMillis = GREEN_DELAY;
                     }
                     else if (4 == stateBeforeEm || 5 == stateBeforeEm) {
                         stateRegular = 1;
-                        remainingMillis=GREEN_DELAY;
+                        remainingMillis = GREEN_DELAY;
                     }
                     else {
                         stateRegular = stateBeforeEm;
-                        remainingMillis=RED_DELAY;  //not sure deste tempo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        remainingMillis = RED_DELAY;
                     }
                     Emergency = FALSE;
                 }
@@ -216,8 +230,9 @@ int main() {
                 break;
             default:
                 stateRegular = BREAKDOWN_ENTRY_STATE;
-                remainingMillis=BREAKDOWN_DELAY;
+                remainingMillis = BREAKDOWN_DELAY;
                 disableExternalInputs();
+                Emergency = FALSE;
                 break;
         }
     }
